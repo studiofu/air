30be25e614e21787e0a1e27beea6c16bbd119496
Johnson Fu <johnson.fu@gmail.com>
2023-06-02 23:47:13 +0800

    create register api

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -14,7 +14,7 @@ const Logo = () => {
             alt="Logo"
             className="hidden md:block cursor-pointer"
             height="100"
-            width="100"
+            width="100"            
             src="/images/logo.png"
         />
     );
